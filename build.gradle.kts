3ca595ff596a74198aa59d7e9257de8a3eb6ef26
filippo16 <filippo.lutterotti2@gmail.com>
2026-01-10 16:20:25 +0100

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    // Apply the java plugin to add support for Java
+    java
+
+    // Apply the application plugin to add support for building a CLI application
+    // You can run your app via task "run": ./gradlew run
+    application
+    id("org.javamodularity.moduleplugin") version "2.0.0"
+    /*
+     * Adds tasks to export a runnable jar.
+     * In order to create it, launch the "shadowJar" task.
+     * The runnable jar will be found in build/libs/projectname-all.jar
+     */
+    id("com.gradleup.shadow") version "9.3.1"
+}
+
+repositories {
+    mavenCentral()
+}
+
+val javaFXModules = listOf(
+    "base",
+    "controls",
+    "fxml",
+    "swing",
+    "graphics"
+)
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+val javaFxVersion = 25
+val supportedPlatforms = listOf("linux", "mac", "win")
+
+dependencies {
+    // Suppressions for SpotBugs
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.8")
+    val jUnitVersion = "6.0.2"
+    // JUnit API and testing engine
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+    for (platform in supportedPlatforms) {
+        for (module in javaFXModules) {
+            implementation("org.openjfx:javafx-$module:$javaFxVersion:$platform")
+        }
+    }
+}
+
+tasks.withType<Test> {
+    // Enables JUnit 5 Jupiter module
+    useJUnitPlatform()
+}
+
+application {
+    // Define the main class for the application
+    mainClass.set("it.unibo.samplejavafx.base.AppLauncher")
+}
