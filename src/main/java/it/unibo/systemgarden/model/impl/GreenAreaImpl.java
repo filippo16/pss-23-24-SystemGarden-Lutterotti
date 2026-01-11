f225da71557f2669c12ebf5305ab6ab0de467727
filippo16 <filippo.lutterotti2@gmail.com>
2026-01-11 18:54:40 +0100

    add: GreenArea and Sector implementation

--- src/main/java/it/unibo/systemgarden/model/impl/GreenAreaImpl.java
+++ src/main/java/it/unibo/systemgarden/model/impl/GreenAreaImpl.java
@@ -1,6 +1,42 @@
 package it.unibo.systemgarden.model.impl;
 
+import it.unibo.systemgarden.model.api.GreenArea;
 
-public class GreenAreaImpl {
-    
+import java.util.Random;
+
+/**
+ * Implementation of GreenArea interface.
+ */
+public class GreenAreaImpl implements GreenArea {
+
+    private final String id;
+    private final String name;
+    private final String city;
+
+    /**
+     * Creates a new green area.
+     * 
+     * @param name the area name
+     * @param city the city where the area is located
+     */
+    public GreenAreaImpl( final String name, final String city ) {
+        this.id = "AREA-" + new Random().nextInt( 100000 );
+        this.name = name;
+        this.city = city;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getCity() {
+        return this.city;
+    }
 }
