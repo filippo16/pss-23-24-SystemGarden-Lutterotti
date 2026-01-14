6201c2ea0773151f9a086be0158a91cd1ab46eb0
filippo16 <filippo.lutterotti2@gmail.com>
2026-01-14 10:26:27 +0100

    View with FXML init - add first base file

---
+++ src/main/java/it/unibo/systemgarden/view/impl/MainViewHandler.java
@@ -0,0 +1,18 @@
+package it.unibo.systemgarden.view.impl;
+
+import javafx.fxml.FXML;
+
+
+/**
+ * Handler for the main view FXML. 
+ * Manages the methods called in the view for buttons and other components. 
+ */
+public class MainViewHandler {
+
+    @FXML
+    private void onAddAreaClicked() {
+        System.out.println("Add Area button clicked");
+    }
+
+   
+}
