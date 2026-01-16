25e63d1013c59d09e219f5d5230648c3d6b7c4e1
filippo16 <filippo.lutterotti2@gmail.com>
2026-01-16 15:42:03 +0100

    Add location file

---
+++ src/main/java/it/unibo/systemgarden/model/api/Location.java
@@ -0,0 +1,5 @@
+package it.unibo.systemgarden.model.api;
+
+public interface Location {
+    
+}
---
+++ src/main/java/it/unibo/systemgarden/model/impl/LocationImpl.java
@@ -0,0 +1,7 @@
+package it.unibo.systemgarden.model.impl;
+
+import it.unibo.systemgarden.model.api.Location;
+
+public class LocationImpl implements Location {
+    
+}
