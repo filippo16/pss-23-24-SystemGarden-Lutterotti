c5fd37a0105e1e88f2428e7dac7972d8c03fae16
filippo16 <filippo.lutterotti2@gmail.com>
2026-01-19 15:02:12 +0100

    Modify card controller api to a generic one

--- src/main/java/it/unibo/systemgarden/view/api/CardController.java
+++ src/main/java/it/unibo/systemgarden/view/api/CardController.java
@@ -1,15 +1,14 @@
 package it.unibo.systemgarden.view.api;
 
 import it.unibo.systemgarden.controller.api.Controller;
-import it.unibo.systemgarden.model.api.GreenArea;
 import javafx.scene.layout.VBox;
 
-public interface CardController {
+public interface CardController<T> {
 
     /**
      * Initializes the card with data.
      */
-    void initialize(final Controller controller, final GreenArea area, final String css);
+    void initialize(Controller controller, T data, String css);
 
     VBox getCard();
 }
