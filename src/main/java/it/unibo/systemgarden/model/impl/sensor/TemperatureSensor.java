6c2ae502327c95c0f67a146a29f805ffab8338e9
filippo16 <filippo.lutterotti2@gmail.com>
2026-01-22 17:01:42 +0100

    add generation function for specific sensors

--- src/main/java/it/unibo/systemgarden/model/impl/sensor/TemperatureSensor.java
+++ src/main/java/it/unibo/systemgarden/model/impl/sensor/TemperatureSensor.java
@@ -5,6 +5,9 @@ import it.unibo.systemgarden.model.utils.SensorType;
 public class TemperatureSensor extends AbstractSensor {
 
     private static final SensorType TYPE = SensorType.TEMPERATURE;
+    private static final double MIN_TEMP = 5.0;
+    private static final double MAX_TEMP = 40.0;
+    private static final double VARIATION = 2.0;
 
     public TemperatureSensor(String name) {
         super(name);
@@ -15,10 +18,21 @@ public class TemperatureSensor extends AbstractSensor {
         return TYPE;
     }
 
-    // ONLY TEST METHOD
-    public void setTemperature( double temperature ) {
-        this.currentValue = temperature;
-        notifyObservers();
+    @Override
+    protected double generateNewValue() {
+        
+        double variation = (getRandom().nextDouble() - 0.5) * VARIATION * 2;
+        double newValue = this.currentValue + variation;
+
+        
+        if (newValue < MIN_TEMP) {
+            newValue = MIN_TEMP;
+        }
+        if (newValue > MAX_TEMP) {
+            newValue = MAX_TEMP;
+        }
+
+        return Math.round(newValue * 10) / 10.0;
     }
     
 }
