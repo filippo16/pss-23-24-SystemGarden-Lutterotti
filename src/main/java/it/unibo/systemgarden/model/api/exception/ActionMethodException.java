127c739990281400f4d66ebc22294e54b1a8ea6c
filippo16 <filippo.lutterotti2@gmail.com>
2026-01-23 11:51:31 +0100

    Add custom exception

---
+++ src/main/java/it/unibo/systemgarden/model/api/exception/ActionMethodException.java
@@ -0,0 +1,8 @@
+package it.unibo.systemgarden.model.api.exception;
+
+public class ActionMethodException extends Exception {
+    
+    public ActionMethodException(String message) {
+        super(message);
+    }
+}
