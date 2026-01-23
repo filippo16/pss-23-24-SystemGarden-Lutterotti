f2db1828b35e03306ce23736aeb9ef47b84e8327
filippo16 <filippo.lutterotti2@gmail.com>
2026-01-23 10:45:47 +0100

    add display method

--- src/main/java/it/unibo/systemgarden/view/component/ToastController.java
+++ src/main/java/it/unibo/systemgarden/view/component/ToastController.java
@@ -1,6 +1,7 @@
 package it.unibo.systemgarden.view.component;
 
 import it.unibo.systemgarden.view.utils.ToastType;
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -8,10 +9,12 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 public class ToastController {
     
     private static final String FXML_PATH = "fxml/component/Toast.fxml";
+    private static final int DISPLAY_TIME_MS = 3000;
 
     @FXML
     private StackPane toastRoot;
@@ -42,9 +45,17 @@ public class ToastController {
         
 
         toastStage.setOnShown(e -> {
-            toastStage.setX(owner.getX() + ( owner.getWidth() - toastStage.getWidth() ) / 2);
-            toastStage.setY(owner.getY() + owner.getHeight() - 100);
+            toastStage.setX( owner.getX() + ( owner.getWidth() - toastStage.getWidth() ) / 2 );
+            toastStage.setY( owner.getY() + owner.getHeight() - 100 );
         });
+    }
+
+    private void display() {
+        toastStage.show();
+        
+        PauseTransition delay = new PauseTransition( Duration.millis( DISPLAY_TIME_MS ) );
+        delay.setOnFinished( e -> toastStage.close() );
+        delay.play();
     }
 
 }
