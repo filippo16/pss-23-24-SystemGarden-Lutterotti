702092db794fb2aac5ea52f25998ef48b7d6458d
filippo16 <filippo.lutterotti2@gmail.com>
2026-01-23 10:43:11 +0100

    Add styleclass for a specific type

--- src/main/java/it/unibo/systemgarden/view/utils/ToastType.java
+++ src/main/java/it/unibo/systemgarden/view/utils/ToastType.java
@@ -1,8 +1,18 @@
 package it.unibo.systemgarden.view.utils;
 
 public enum ToastType {
-    SUCCESS,
-    ERROR,
-    WARNING,
-    INFO
+    SUCCESS("toast-success"),
+    ERROR("toast-error"),
+    WARNING("toast-warning"),
+    INFO("toast-info");
+
+    private final String styleClass;
+
+    ToastType(String styleClass) {
+        this.styleClass = styleClass;
+    }
+
+    public String getStyleClass() {
+        return styleClass;
+    }
 }
