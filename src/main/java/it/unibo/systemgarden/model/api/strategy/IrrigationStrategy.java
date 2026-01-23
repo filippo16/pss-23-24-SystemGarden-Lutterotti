6283b5d7505ad7f665763288be8a3ebc6080363c
filippo16 <filippo.lutterotti2@gmail.com>
2026-01-23 15:55:54 +0100

    Add a strategy interface

---
+++ src/main/java/it/unibo/systemgarden/model/api/strategy/IrrigationStrategy.java
@@ -0,0 +1,19 @@
+package it.unibo.systemgarden.model.api.strategy;
+
+import it.unibo.systemgarden.model.utils.IrrigationAdvice;
+
+
+/**
+ * Interface for irrigation advisors.
+ * Implements a strategy pattern to provide irrigation advice based on environmental conditions.
+*/
+public interface IrrigationStrategy {
+     /**
+     * Analyzes the current conditions and returns an irrigation advice.
+     * 
+     * @param humidity    the current humidity percentage (0-100)
+     * @param temperature the current temperature in Celsius
+     * @return the irrigation advice based on the conditions
+    */
+    IrrigationAdvice getAdvice(double humidity, double temperature);
+}
